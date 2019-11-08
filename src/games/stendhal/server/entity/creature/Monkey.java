7e6d356daaf6ec15a8d7288fed11011c07ff2985
Josie Lambert <josephine.lambert-2@student.manchester.ac.uk>
2019-11-08 15:42:08 +0000

    Create new class Monkey.
    New class Monkey made which extends Pet, to be the stealing Pet.

---
+++ src/games/stendhal/server/entity/creature/Monkey.java
@@ -0,0 +1,19 @@
+package games.stendhal.server.entity.creature;
+
+import java.util.List;
+
+public class Monkey extends Pet {
+
+	@Override
+	protected List<String> getFoodNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void setUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
