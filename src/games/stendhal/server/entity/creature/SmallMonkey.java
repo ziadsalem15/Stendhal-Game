5bfe049ab991feb98145f338f0baa2d9efdd7b63
Chelsea Leach <chelsea.leach@student.manchester.ac.uk>
2019-11-11 14:27:49 +0000

    Updated smallmonkey.png for all directions to stop glitch in graphics

--- src/games/stendhal/server/entity/creature/SmallMonkey.java
+++ src/games/stendhal/server/entity/creature/SmallMonkey.java
@@ -1,10 +1,17 @@
 package games.stendhal.server.entity.creature;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.core.pathfinder.Path;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.npc.NPC;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPClass;
 import marauroa.common.game.RPObject;
@@ -30,7 +37,8 @@ public class SmallMonkey extends Pet {
 		setBaseHP(HP);
 		setHP(HP);
 	}
-
+	NPC stealTarget = null;
+	
 	public static void generateRPClass() {
 		try {
 			final RPClass smallmonkey = new RPClass("smallmonkey");
@@ -97,6 +105,113 @@ public class SmallMonkey extends Pet {
 	protected boolean takesPartInCombat() {
 		return false;
 	}
+
+	//find new target
+	
+	/**
+	 * Returns a list of targets. One of it will be stolen from.
+	 *
+	 * @return list of targets
+	 */
+	public List<NPC> getTargetList() {
+			List<NPC> returnList = getZone().getNPCList();
+			if (owner != null) 
+				returnList.remove(owner);
+			return returnList;
+			
+	}
+
+	/**
+	 * Returns the nearest target, which is reachable or otherwise attackable.
+	 *
+	 * @param range
+	 *            attack radius
+	 * @return chosen enemy or null if no enemy was found.
+	 */
+	public NPC getNearestTarget(final double range) {
+		// create list of enemies
+		final List<NPC> targetList = getTargetList();
+		if (targetList.isEmpty()) {
+			return null;	
+		}
+		// calculate the distance of all possible enemies
+		final Map<NPC, Double> distances = new HashMap<NPC, Double>();
+		for (final RPEntity target : targetList) {
+			if (target == this) {
+				continue;
+			}
+
+			if (target.isInvisibleToCreatures()) {
+				continue;
+			}
+			final double squaredDistance;
+			if (owner != null) {
+				squaredDistance = owner.squaredDistance(target);}
+			else {
+				squaredDistance = this.squaredDistance(target);}
+			
+			if (squaredDistance <= (range * range)) {
+				distances.put((NPC) target, squaredDistance);
+			}
+		}
+
+		// now choose the nearest target for which there is a path, or is
+		// attackable otherwise
+		NPC chosen = null;
+		while ((chosen == null) && !distances.isEmpty()) {
+			double shortestDistance = Double.MAX_VALUE;
+			for (final Map.Entry<NPC, Double> target : distances.entrySet()) {
+				final double distance = target.getValue();
+				if (distance < shortestDistance) {
+					chosen = target.getKey();
+					shortestDistance = distance;
+				}
+			}
+
+			if (shortestDistance >= 1) {
+				final List<Node> path = Path.searchPath(this, chosen, getMovementRange());
+				if ((path == null) || path.isEmpty()) {
+					distances.remove(chosen);
+					chosen = null;
+				} else {
+					// set the path. if not setMovement() will search a new one
+					setPath(new FixedPath(path, false));
+				}
+			}
+		}
+		// return the chosen enemy or null if we could not find one in reach
+		return chosen;
+	}
+	
+	/**
+	 * Determines what the pet shall do next.
+	 */
+	@Override
+	public void logic() {
+		// call super class to perform common tasks like attacking targets
+		super.logic();
+		
+		
+		//if owner isn't doing anything and in perception range then do this
+		if (//System.getProperty("stendhal.petleveling", "false").equals("true") this never runs!!
+				!takesPartInCombat() 
+				&& (owner != null)
+				&& (Math.sqrt(this.squaredDistance(owner)) < (this.getPerceptionRange()-5))
+				){
+			stealTarget = this.getNearestTarget(this.getPerceptionRange()+2);
+			this.setTarget(stealTarget); //monkey is attacking target!!
+			//this.setIdea("idle"); doesn't stop it attacking its target!
+			if(Math.sqrt(this.squaredDistance(owner)) < 10)
+			{
+				if (!nextTo(stealTarget) ) {
+					clearPath();
+					this.setMovement(stealTarget, 0, 0, this.getMovementRange());
+					this.applyMovement();
+				}
+			}
+			else moveToOwner();
+		}	
+	}//logic
 
 
 }
