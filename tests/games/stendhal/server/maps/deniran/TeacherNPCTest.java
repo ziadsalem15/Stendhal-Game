9eaadd9cfb4298a194ea823b5724a33db0317eef
rares ion <rares.ion@student.manchester.ac.uk>
2019-11-12 11:51:22 +0000

    Created Lon Jatham NPC, and added features to the test.
    After creating the NPC i edited the xml file of deniran to add him in
    the castle there. The hi and bye test passed and then I added an extra
    test for quest hellp and job that also passed. Lon Jatham is now able 
    to interact reliably.

--- tests/games/stendhal/server/maps/deniran/TeacherNPCTest.java
+++ tests/games/stendhal/server/maps/deniran/TeacherNPCTest.java
@@ -23,10 +23,11 @@ public class TeacherNPCTest extends ZonePlayerAndNPCTestImpl {
 	public TeacherNPCTest() {
 		setNpcNames("Lon Jatham");
 		setZoneForPlayer(ZONE_NAME);
+		addZoneConfigurator(new TeacherNPC(), ZONE_NAME);
 	}
 
 	/**
-	 * Tests for hiAndBye.
+	 * Tests for dialogue.
 	 */
 	@Test
 	public void testHiAndBye() {
@@ -35,9 +36,22 @@ public class TeacherNPCTest extends ZonePlayerAndNPCTestImpl {
 
 		assertTrue(en.step(player, "hi Lon"));
 		assertEquals("Hello curious mind!", getReply(npc));
-
+	   }
+	
+	public void testQuestHelpAndJob() {
+		final SpeakerNPC npc = getNPC("Lon Jatham");
+		final Engine en = npc.getEngine();
+		
+		assertTrue(en.step(player, "quest"));
+		assertEquals("Find me 3 students that are willing to enroll.", getReply(npc));
+		
+		assertTrue(en.step(player, "help"));
+		assertEquals("Ask me #quest  to learn about my quest of finding new young minds eager to learn.", getReply(npc));
+		
+		assertTrue(en.step(player, "job"));
+		assertEquals("I am a professor of the mystic language of java!", getReply(npc));
+		
 		assertTrue(en.step(player, "bye"));
 		assertEquals("See you at the next lecture!", getReply(npc));
-		
 	}
 }
