52920a281bc020c3c81ff351d7607c014c1439d1
rares ion <rares.ion@student.manchester.ac.uk>
2019-12-03 13:58:28 +0000

    Create a test for Achievement class
    The test has a coverage higher than 60% and checks the constructor, base
    scores and methods of the Achievement class.

---
+++ tests/games/stendhal/server/core/rp/achievement/AchivementTest.java
@@ -0,0 +1,52 @@
+package games.stendhal.server.core.rp.achievement;
+
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+
+import games.stendhal.server.entity.npc.condition.QuestStateGreaterThanCondition;
+
+
+public class AchivementTest {
+    
+	Category c =  Category.EXPERIENCE;
+	Achievement a = new Achievement("some identifier", "some title", c, "some description", 
+            Achievement.EASY_BASE_SCORE, true, new QuestStateGreaterThanCondition("daily_item", 2, 9));
+	
+	
+
+
+	@Test
+	public void testBaseScores() {
+		assertEquals(Achievement.EASY_BASE_SCORE, 1);
+		assertEquals(Achievement.MEDIUM_BASE_SCORE, 2);
+		assertEquals(Achievement.HARD_BASE_SCORE, 5);
+		
+	}
+	
+	@Test
+	public void testGetters() {
+		assertEquals(a.getIdentifier(),"some identifier");
+		assertEquals(a.getTitle(),"some title");
+		assertEquals(a.getCategory(),Category.EXPERIENCE);
+		assertEquals(a.getDescription(),"some description"); 
+		assertEquals(a.getBaseScore(), Achievement.EASY_BASE_SCORE);
+	}
+	
+	@Test
+	public void testBooleans() {
+		assertEquals(a.isActive(), true);
+		
+		
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals(a.toString(),"Achievement<id: "+"some identifier"+", title: "+"some title"+">");
+		
+	}
+
+}
