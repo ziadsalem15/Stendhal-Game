972da0aca79e51b5cf6aa643944fae1eeb234a3f
rares ion <rares.ion@student.manchester.ac.uk>
2019-12-03 17:32:07 +0000

    Update AchievementNotifierTest
    Add methods to AchievementNotifierTest 
    and it has a coverage of over 60%.

--- tests/games/stendhal/server/core/rp/achievement/AchievmentNotifierTest.java
+++ tests/games/stendhal/server/core/rp/achievement/AchievmentNotifierTest.java
@@ -1,7 +1,8 @@
 package games.stendhal.server.core.rp.achievement;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
+import com.google.common.collect.ImmutableList;
 import games.stendhal.server.core.rp.achievement.AchievementNotifier;
 import games.stendhal.server.entity.player.Player;
 import utilities.PlayerTestHelper;
@@ -13,11 +14,39 @@ public class AchievmentNotifierTest {
 	public void initializeTest()
 	{
 		AchievementNotifier instance1 = AchievementNotifier.get();
-		instance1.initialize();		
+		instance1.initialize();	
+	}
+	@Test
+	public void onItemLootTest()
+	{
+		AchievementNotifier instance2 = AchievementNotifier.get();
 		final Player player = PlayerTestHelper.createPlayer("player");
-		instance1.onItemLoot(player);
-		String achievementIdentifier = null;
-		instance1.awardAchievementIfNotYetReached(player, achievementIdentifier);
+		instance2.onItemLoot(player);
 	}
 	
+	@Test
+	public void awardAchievementTest()
+	{	
+		AchievementNotifier instance3 = AchievementNotifier.get();
+		String achievementIdentifier = null;
+		final Player player2 = PlayerTestHelper.createPlayer("player");
+		instance3.awardAchievementIfNotYetReached(player2, achievementIdentifier);
+	}
+	
+	@Test
+	public void onLoginTest()
+	{
+		AchievementNotifier instance4 = AchievementNotifier.get();
+		final Player player3 = PlayerTestHelper.createPlayer("player");
+		instance4.onLogin(player3);
+	}
+	
+	@Test
+	public void getAchievementsTest()
+	{
+		AchievementNotifier instance4 = AchievementNotifier.get();
+		ImmutableList <Achievement> achievmentsList = instance4.getAchievements() ;
+		assertEquals(instance4.getAchievements(),achievmentsList);
+		
+	}
 }
